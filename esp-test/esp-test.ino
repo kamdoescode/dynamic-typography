0125705e8304bc8d24d247622d58b3ac96ccada4
kamdoescode <kamryn.hanley@gmail.com>
2025-05-16 15:57:15 +0100

    initializing & registering URI handlers

--- esp-test/esp-test.ino
+++ esp-test/esp-test.ino
@@ -1,5 +1,23 @@
 #include <WiFi.h>
 
+//http get request for webpage
+httpd_uri_t uri_get = {
+    .uri = "/",  //the path
+    .method = HTTP_GET,  //the http method
+    .handler = get_req_handler , //pointer to function that will handle requests to this URI
+    .user_ctx = NULL //user context data
+};
+
+//get request at /ws and handles WebSocket connections
+httpd_uri_t ws = {
+  .uri = "/ws",
+  .method = HTTP_GET,
+  .handler = handle_ws_req,
+  .user_ctx = NULL,
+  .is_websocket = true //marks as websocket handler
+};
+
+
 const char* ssid = "Kamryn";
 const char* password = "zj658hbcnv8tt";
 
@@ -11,7 +29,16 @@ void setup() {
     Serial.println("Connecting to WiFi..");
   }
   Serial.println(WiFi.localIP());
+
+  //registering handlers
+  if (httpd_start(&server, &config) == ESP_OK) {
+    httpd_register_uri_handler(server, &uri_get);
+    httpd_register_uri_handler(server, &ws);
+  }
 }
+
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
